121c0dd566cdeec3f9fdbcdd9f2742ba22fd0a3c
Wolfpack075 <kazifahimtahmid@gmail.com>
2023-10-10 00:52:19 +0600

    Initial Commit

---
+++ login/loginApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginApp.swift
+//  login
+//
+//  Created by Shoumik Barman Polok on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct loginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
