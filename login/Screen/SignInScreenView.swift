9f8fc90286ddea29ef6dd6300acdc0751cad68dd
Wolfpack075 <kazifahimtahmid@gmail.com>
2023-10-10 02:30:11 +0600

    Screen Issues fixed

---
+++ login/Screen/SignInScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  SignInScreenView.swift
+//  login
+//
+//  Created by Kazi Fahim Tahmid on 10/10/23.
+//
+
+import SwiftUI
+
+struct SignInScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SignInScreenView()
+}
---
+++ login/Screen/WelcomeScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  WelcomeScreenView.swift
+//  login
+//
+//  Created by Kazi Fahim Tahmid on 10/10/23.
+//
+
+import SwiftUI
+
+struct WelcomeScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WelcomeScreenView()
+}
